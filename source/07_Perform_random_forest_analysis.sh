676c5be356193855d4703403f35ae3db4e2d5e58
Johannes Asplund Samuelsson <johannes.aspsam@gmail.com>
2020-08-21 12:29:34 +0200

    Removed enzyme alignment analysis from random forest analysis

--- source/07_Perform_random_forest_analysis.sh
+++ source/07_Perform_random_forest_analysis.sh
@@ -4,7 +4,7 @@
 DATA="EC_count_features"
 echo "Using ${DATA}"
 
-source/10_Rank_features_with_random_forest.py \
+source/Rank_features_with_random_forest.py \
   --features      intermediate/${DATA}.X.csv.gz \
   --classes       intermediate/${DATA}.y.txt \
   --feature_names intermediate/${DATA}.feature_names.txt \
@@ -19,31 +19,11 @@ source/10_Rank_features_with_random_forest.py \
 # Compress output
 pigz intermediate/${DATA}.importance.tab intermediate/${DATA}.prediction.tab
 
-# Perform random forest analysis using alignment features
-DATA="alignment_features"
-echo "Using ${DATA}"
-
-source/10_Rank_features_with_random_forest.py \
-  --features      intermediate/${DATA}.X.csv.gz \
-  --classes       intermediate/${DATA}.y.txt \
-  --feature_names intermediate/${DATA}.feature_names.txt \
-  --accession_ids intermediate/${DATA}.accession_ids.txt \
-  --taxonomy      intermediate/accession_taxonomy.tab \
-  --importances   intermediate/${DATA}.importance.tab \
-  --predictions   intermediate/${DATA}.prediction.tab \
-  --phy_fil \
-  --var_cut \
-  >               intermediate/${DATA}.random_forest.log \
-  2>              intermediate/${DATA}.random_forest.error
-
-# Compress output
-pigz intermediate/${DATA}.importance.tab intermediate/${DATA}.prediction.tab
-
 # Perform random forest analysis using Pfam features
 DATA="pfam_features"
 echo "Using ${DATA}"
 
-source/10_Rank_features_with_random_forest.py \
+source/Rank_features_with_random_forest.py \
   --features      intermediate/${DATA}.X.csv.gz \
   --classes       intermediate/${DATA}.y.txt \
   --feature_names intermediate/${DATA}.feature_names.txt \
